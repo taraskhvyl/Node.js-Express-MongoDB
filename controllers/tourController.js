52f95f276567f89f71fde24b986c4098ec283c1b
Taras <taras.khvyl@gmail.com>
2023-01-28 13:23:22 +0100

    WIP: Refactoring utils

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require("./../models/tourModel")
+const APIFeatures = require("../utils/apiFeatures");
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -10,48 +11,15 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = {...req.query};
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
-
-    let queryString = JSON.stringify(queryObj);
-    queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-
-    let query = Tour.find(JSON.parse(queryString));
-
-    // SORTING
-    if(req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // FIELD LIMITING
-    if(req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v')
-    }
-
-    // PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skipValue = (page - 1) * limit;
-
-    query = query.skip(skipValue).limit(limit);
-
-    if(req.query.page) {
-      const numTours = await Tour.countDocuments();
-
-      if(skipValue >= numTours) {
-        throw new Error('This page doesn\'t exist');
-      }
-    }
 
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limit()
+      .paginate();
+
+    const tours = await features.query;
   
     res.status(200).json({
       status: 'success',
