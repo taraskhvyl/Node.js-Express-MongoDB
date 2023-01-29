6562770d77691651fb1d27d073c3d66f84e5ef83
Taras <taras.khvyl@gmail.com>
2023-01-29 19:18:17 +0100

    WIP: Data validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "A tour must have a name"],
     unique: true,
-    trim: true
+    trim: true,
+    maxlength: [40, "A tour should be less than 40 symbols"],
+    minlength: [10, "A tour should be more than 10 symbols"]
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, "A tour must have a duration"]
@@ -17,11 +21,17 @@ const tourSchema = new mongoose.Schema({
   },
   difficulty: {
     type: String,
-    required: [true, "A tour must have a difficulty"]
+    required: [true, "A tour must have a difficulty"],
+    enum: {
+      values: ["easy", "medium", "difficult"],
+      message: "Value is not correct"
+    }
   },
   ratingsAverage: {
     type: Number,
-    default: 4.5
+    default: 4.5,
+    min: [1, "Rating must be more than 1.0"],
+    max: [5, "Rating must be below 5.0"]
   },
   ratingsQuantity: {
     type: Number,
@@ -51,8 +61,41 @@ const tourSchema = new mongoose.Schema({
     default: Date.now(),
     select: false
   },
+  secretTour: {
+    type: Boolean,
+    default: false
+  },
   startDates: [Date]
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
 });
+
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7;
+});
+
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, {lower: true});
+
+  next();
+});
+
+tourSchema.pre(/^find/, function(next) {
+  this.find({
+    secretTour: { $ne: true }
+  });
+
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next) {
+  console.log(this)
+
+  next();
+})
+
 const Tour = mongoose.model('Tour', tourSchema)
 
 module.exports = Tour;
